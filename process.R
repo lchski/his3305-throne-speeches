8fe4e97ec9ef0b237da3ec510dbd90b9eb97f7e0
Lucas Cherkewski <lucas@ecustom.ca>
2018-11-23 08:33:07 -0500

    initial commit

---
+++ process.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+
+# Load base data
+index_budget <- read_csv("data/source/index-budget.csv")
+index_throne <- read_csv("data/source/index-throne-speech.csv")
+dates_parliaments <- read_csv("data/source/dates-parliaments.csv")
+dates_sessions <- read_csv("data/source/dates-parliaments-sessions.csv")
+
+# Remove unnecessary columns
+index_budget <- index_budget %>%
+  select(
+    parliament = `Parliament`,
+    type = `Type of Budget`,
+    date = `Date of Speech`,
+    minister = `Minister of Finance`
+  ) %>%
+  mutate(
+    parliament = as.numeric(str_extract(parliament, "(\\d+)"))
+  )
+
+index_throne <- index_throne %>%
+  select(
+    parliament = `Parliament`,
+    session = `Session`,
+    date = `Debates`
+  ) %>%
+  mutate(
+    parliament = as.numeric(str_extract(parliament, "(\\d+)")),
+    session = as.numeric(str_extract(session, "(\\d+)")),
+    date = as.Date(substr(date, 1, 10))
+  )
+
+# Filter to relevant parliaments (23 to 41, 1957-2015)
+index_budget <- index_budget %>%
+  filter(parliament > 22) %>%
+  filter(parliament < 42)
+
+index_throne <- index_throne %>%
+  filter(parliament > 22) %>%
+  filter(parliament < 42)
