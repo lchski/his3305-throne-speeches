ba9b76d220447f0b005928a8ad984d07a36dba4d
Lucas Cherkewski <lucas@ecustom.ca>
2018-11-25 14:12:54 -0500

    print all decades, centre labels, create experimentation graph

--- visualize.R
+++ visualize.R
@@ -39,21 +39,70 @@ plot_avg_weights_by_decade_by_party <- topic_probabilities_by_document %>%
   geom_smooth(method = "lm", se = 0, colour = "black", linetype = "dashed", size = 0.5) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, NA)) +
   scale_fill_manual(values = c("conservative" = "blue", "liberal" = "red")) +
+  scale_x_date(
+    limits = as.Date(c("1945-01-01","2015-01-01")),
+    breaks = as.Date(c(
+      "1950-01-01",
+      "1960-01-01",
+      "1970-01-01",
+      "1980-01-01",
+      "1990-01-01",
+      "2000-01-01",
+      "2010-01-01"
+    )),
+    minor_breaks = NULL,
+    date_labels = "%Y"
+  ) +
   facet_wrap(~ topic_label) +
   theme(
     strip.text.x = element_text(hjust = 0),
-    axis.text.x = element_text(angle = 90, hjust = 1)
+    axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)
+  )
+
+### [single graph to experiment with]
+topic_probabilities_by_document %>%
+  group_by(topic_label, decade = as.Date(paste(substr(date, 1, 3), "0-01-01", sep="")), governing_party) %>%
+  summarize(avg_weight = mean(weight)) %>%
+  ungroup() %>%
+  filter(substr(topic_label, 1, 2) == "1 ") %>%
+  ggplot(mapping = aes(x = decade, y = avg_weight)) +
+  geom_col(mapping = aes(fill = governing_party), position = "dodge") +
+  geom_smooth(method = "lm", se = 0, colour = "black", linetype = "dashed", size = 0.5) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, NA)) +
+  scale_fill_manual(values = c("conservative" = "blue", "liberal" = "red")) +
+  scale_x_date(
+    limits = as.Date(c("1945-01-01","2015-01-01")),
+    breaks = as.Date(c(
+      "1950-01-01",
+      "1960-01-01",
+      "1970-01-01",
+      "1980-01-01",
+      "1990-01-01",
+      "2000-01-01",
+      "2010-01-01"
+    )),
+    minor_breaks = NULL,
+    date_labels = "%Y"
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)
   )
 
 
+
+topic_probabilities_by_document %>%
+  group_by(topic_id, decade = as.Date(paste(substr(date, 1, 3), "0-01-01", sep="")), governing_party, topic_label) %>%
+  summarize(avg_weight = mean(weight)) %>%
+  ungroup() %>%
+  mutate(topic_label = fct_reorder(topic_label, topic_id))
 
 # Save summaries
 
 plot_avg_weights_by_decade_by_party +
   ggsave(
     paste("data/models/summaries/", conf_num_topics, "-", conf_num_runs, "-1-avg-weights-decade-party.pdf", sep=""),
-    width = 11,
-    height = 8.5,
+    width = 17,
+    height = 11,
     units = "in"
   )
 
