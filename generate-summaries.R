968bce857f0bda66ed8c1b0dee363e781eaa549a
Lucas Cherkewski <lucas@ecustom.ca>
2018-11-25 16:39:31 -0500

    summary generator for each saved model

--- generate-summaries.R
+++ generate-summaries.R
@@ -1,12 +1,32 @@
-source(visualize.R)
+library(tidyverse)
+library(dfrtopics)
+library(mallet)
 
-plot_avg_weights_by_decade_by_party +
-  ggsave(
-    paste("data/models/summaries/", conf_num_topics, "-", conf_num_runs, "-1-avg-weights-decade-party.pdf", sep=""),
-    width = 17,
-    height = 11,
-    units = "in"
-  )
+# Bring in visualization functions
+source("visualize.R")
 
-topic_labels %>% write_csv(paste("data/models/summaries/", conf_num_topics, "-", conf_num_runs, "-1-topic-labels.csv", sep=""))
+# Create list of paths to the saved models
+models <- tibble(file = dir(path = "data/models/", full.names = TRUE)) %>%
+  filter(str_detect(file, ".RData")) %>%
+  mutate(file = gsub("//", "/", file))
 
+# Generate a summary of each model
+by(models, 1:nrow(models), function(model_path) {
+  # Load the model data into the global environment [wooo global variables, heh]
+  load(as.character(model_path), envir = globalenv())
+  
+  # Create a base filename for each summary file
+  base_filename <- paste0("data/models/summaries/", conf_num_topics, "-", conf_num_runs, "-1-")
+  
+  # Save the topic labels
+  topic_labels %>% write_csv(paste0(base_filename, "topic-labels.csv"))
+    
+  # Save the plot of average weights by decade by party
+  plot_avg_weights_by_decade_by_party(topic_probabilities_by_document) +
+    ggsave(
+      paste0(base_filename, "avg-weights-decade-party.pdf", sep=""),
+      width = 17,
+      height = 11,
+      units = "in"
+    )
+})
--- visualize.R
+++ visualize.R
@@ -4,8 +4,6 @@ library(mallet)
 
 # NB: first, load a model: `load("data/models/[...].RData")`
 
-# Visualize
-
 ## Topic average weights by party
 plot_avg_weights_by_party <- function(topic_probabilities_by_document) {
   topic_probabilities_by_document %>%
@@ -61,48 +59,3 @@ plot_avg_weights_by_decade_by_party <- function(topic_probabilities_by_document)
       axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)
     )
 }
-
-render_avg_weights_by_decade_by_party <- plot_avg_weights_by_decade_by_party(topic_probabilities_by_document)
-
-### [single graph to experiment with]
-render_exp_graph <- topic_probabilities_by_document %>%
-  group_by(topic_label, decade = as.Date(paste(substr(date, 1, 3), "0-01-01", sep="")), governing_party) %>%
-  summarize(avg_weight = mean(weight)) %>%
-  ungroup() %>%
-  filter(substr(topic_label, 1, 2) == "1 ") %>%
-  ggplot(mapping = aes(x = decade, y = avg_weight)) +
-  geom_col(mapping = aes(fill = governing_party), position = "dodge") +
-  geom_smooth(method = "lm", se = 0, colour = "black", linetype = "dashed", size = 0.5) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, NA)) +
-  scale_fill_manual(values = c("conservative" = "blue", "liberal" = "red")) +
-  scale_x_date(
-    limits = as.Date(c("1945-01-01","2015-01-01")),
-    breaks = as.Date(c(
-      "1950-01-01",
-      "1960-01-01",
-      "1970-01-01",
-      "1980-01-01",
-      "1990-01-01",
-      "2000-01-01",
-      "2010-01-01"
-    )),
-    minor_breaks = NULL,
-    date_labels = "%Y"
-  ) +
-  theme(
-    axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)
-  )
-
-
-# Save summaries
-
-plot_avg_weights_by_decade_by_party +
-  ggsave(
-    paste("data/models/summaries/", conf_num_topics, "-", conf_num_runs, "-1-avg-weights-decade-party.pdf", sep=""),
-    width = 17,
-    height = 11,
-    units = "in"
-  )
-
-topic_labels %>% write_csv(paste("data/models/summaries/", conf_num_topics, "-", conf_num_runs, "-1-topic-labels.csv", sep=""))
-
